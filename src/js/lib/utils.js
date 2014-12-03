2ffe9bedc9a7eb7adde7e913d33640c9428c4a0e
Sascha Gehlich <sascha@gehlich.us>
2014-12-03 18:30:57 +0100

    Add Utils#multiplyMatrices (for later use) [skip ci]

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -85,4 +85,40 @@ Utils.shaderString = function (f) {
   return f.toString().split("\n").slice(1, -1).join("\n");
 };
 
+/**
+ * Multiplies the given matrices
+ * @param  {Array} a
+ * @param  {Array} b
+ * @return {Array}
+ */
+Utils.multiplyMatrices = function(a, b) {
+  var a00 = a[0*3+0];
+  var a01 = a[0*3+1];
+  var a02 = a[0*3+2];
+  var a10 = a[1*3+0];
+  var a11 = a[1*3+1];
+  var a12 = a[1*3+2];
+  var a20 = a[2*3+0];
+  var a21 = a[2*3+1];
+  var a22 = a[2*3+2];
+  var b00 = b[0*3+0];
+  var b01 = b[0*3+1];
+  var b02 = b[0*3+2];
+  var b10 = b[1*3+0];
+  var b11 = b[1*3+1];
+  var b12 = b[1*3+2];
+  var b20 = b[2*3+0];
+  var b21 = b[2*3+1];
+  var b22 = b[2*3+2];
+  return [a00 * b00 + a01 * b10 + a02 * b20,
+          a00 * b01 + a01 * b11 + a02 * b21,
+          a00 * b02 + a01 * b12 + a02 * b22,
+          a10 * b00 + a11 * b10 + a12 * b20,
+          a10 * b01 + a11 * b11 + a12 * b21,
+          a10 * b02 + a11 * b12 + a12 * b22,
+          a20 * b00 + a21 * b10 + a22 * b20,
+          a20 * b01 + a21 * b11 + a22 * b21,
+          a20 * b02 + a21 * b12 + a22 * b22];
+};
+
 module.exports = Utils;
