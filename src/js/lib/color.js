7159ba784ce1728a643ed037a592047192316ae6
Sascha Gehlich <sascha@gehlich.us>
2015-03-13 18:34:22 +0100

    :shirt: JSHint: lib/color.js [skip ci]

--- src/js/lib/color.js
+++ src/js/lib/color.js
@@ -118,12 +118,36 @@ class Color {
     let t = v * (1 - (1 - f) * s);
 
     switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+        case 0:
+          r = v;
+          g = t;
+          b = p;
+          break;
+        case 1:
+          r = q;
+          g = v;
+          b = p;
+          break;
+        case 2:
+          r = p;
+          g = v;
+          b = t;
+          break;
+        case 3:
+          r = p;
+          g = q;
+          b = v;
+          break;
+        case 4:
+          r = t;
+          g = p;
+          b = v;
+          break;
+        case 5:
+          r = v;
+          g = p;
+          b = q;
+          break;
     }
 
     this.r = r;
