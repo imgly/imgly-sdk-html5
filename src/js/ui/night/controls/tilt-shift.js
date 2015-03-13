24e061b4b00762a011119dee4c69e11afe75eedb
Sascha Gehlich <sascha@gehlich.us>
2015-03-13 16:54:01 +0100

    TiltShiftControls: Gradient knob is free! :tada: [skip ci]

--- src/js/ui/night/controls/tilt-shift.js
+++ src/js/ui/night/controls/tilt-shift.js
@@ -45,8 +45,6 @@ class TiltShiftControls extends Control {
     this._operationExistedBefore = !!this._ui.operations["tilt-shift"];
     this._operation = this._ui.getOrCreateOperation("tilt-shift");
 
-    // Remember initial identity state
-    this._initialIdentity = this._operation.isIdentity;
     this._initialSettings = {
       start: this._operation.getStart().clone(),
       end: this._operation.getEnd().clone(),
@@ -54,26 +52,24 @@ class TiltShiftControls extends Control {
       blurRadius: this._operation.getBlurRadius()
     };
 
-    this._operation.isIdentity = false;
-
     // Mouse event callbacks bound to the class context
-    this._onKnobDown = this._onKnobDown.bind(this);
-    this._onKnobDrag = this._onKnobDrag.bind(this);
-    this._onKnobUp = this._onKnobUp.bind(this);
+    this._onPositionKnobDown = this._onPositionKnobDown.bind(this);
+    this._onPositionKnobDrag = this._onPositionKnobDrag.bind(this);
+    this._onPositionKnobUp = this._onPositionKnobUp.bind(this);
     this._onGradientKnobDown = this._onGradientKnobDown.bind(this);
     this._onGradientKnobDrag = this._onGradientKnobDrag.bind(this);
     this._onGradientKnobUp = this._onGradientKnobUp.bind(this);
 
+    // Find DOM elements
     let selector = ".imglykit-canvas-tilt-shift-dot";
-    this._startKnob = this._canvasControls.querySelector(`${selector}[data-option="start"]`);
-    this._endKnob = this._canvasControls.querySelector(`${selector}[data-option="end"]`);
+    this._positionKnob = this._canvasControls.querySelector(`${selector}[data-option="position"]`);
     this._gradientKnob = this._canvasControls.querySelector(`${selector}[data-option="gradient"]`);
-    this._knobs = [this._startKnob, this._endKnob];
 
-    this._handleKnobs();
+    // Initialization
     this._initSliders();
 
     this._ui.canvas.render().then(() => {
+      this._handleKnobs();
       this._updateDOM();
     });
   }
@@ -107,18 +103,112 @@ class TiltShiftControls extends Control {
    * @private
    */
   _handleKnobs () {
-    for (let i = 0; i < this._knobs.length; i++) {
-      let knob = this._knobs[i];
-      knob.addEventListener("mousedown", (e) => {
-        this._onKnobDown(knob, e);
-      });
-      knob.addEventListener("touchstart", (e) => {
-        this._onKnobDown(knob, e);
-      });
-    }
-
+    // Add event listeners
+    this._positionKnob.addEventListener("mousedown", this._onPositionKnobDown);
+    this._positionKnob.addEventListener("touchstart", this._onPositionKnobDown);
     this._gradientKnob.addEventListener("mousedown", this._onGradientKnobDown);
     this._gradientKnob.addEventListener("touchstart", this._onGradientKnobDown);
+
+    let canvasSize = this._ui.canvas.size;
+    let { start, end } = this._initialSettings;
+    start = start.clone().multiply(canvasSize);
+    end = end.clone().multiply(canvasSize);
+
+    let dist = end.clone().subtract(start);
+    let middle = start.clone().add(dist.clone().divide(2));
+
+    let totalDist = Math.sqrt(Math.pow(dist.x, 2) + Math.pow(dist.y, 2));
+    let factor = dist.clone().divide(totalDist).divide(2);
+
+    // Calculate initial knob position (middle of start and end)
+    this._knobPosition = middle.clone();
+
+    // Calculate initial gradient knob position
+    let gradientRadius = this._initialSettings.gradientRadius;
+    this._gradientKnobPosition = middle.clone()
+      .add(-gradientRadius * factor.y, gradientRadius * factor.x);
+
+    this._updateStartAndEnd();
+    this._updateDOM();
+
+    this._ui.canvas.render();
+  }
+
+  /**
+   * Calculate start and end positions using the knob positions
+   * @private
+   */
+  _updateStartAndEnd () {
+    let canvasSize = this._ui.canvas.size;
+
+    // Calculate distance between gradient and position knob
+    let diff = this._gradientKnobPosition.clone()
+      .subtract(this._knobPosition);
+
+    let start = this._knobPosition.clone()
+      .add(-diff.y, diff.x)
+      .divide(canvasSize);
+    let end = this._knobPosition.clone()
+      .add(diff.y, -diff.x)
+      .divide(canvasSize);
+
+    this._operation.set({ start, end });
+  }
+
+  /**
+   * Gets called when the user starts dragging the position knob
+   * @param {Event} e
+   * @private
+   */
+  _onPositionKnobDown (e) {
+    e.preventDefault();
+
+    this._initialMousePosition = Utils.getEventPosition(e);
+    this._initialPosition = this._knobPosition.clone();
+    this._initialDistanceToGradientKnob = this._gradientKnobPosition.clone()
+      .subtract(this._initialPosition);
+
+    document.addEventListener("mousemove", this._onPositionKnobDrag);
+    document.addEventListener("touchmove", this._onPositionKnobDrag);
+
+    document.addEventListener("mouseup", this._onPositionKnobUp);
+    document.addEventListener("touchend", this._onPositionKnobUp);
+  }
+
+  /**
+   * Gets called when the user drags the position knob
+   * @param {Event} e
+   * @private
+   */
+  _onPositionKnobDrag (e) {
+    e.preventDefault();
+
+    let mousePosition = Utils.getEventPosition(e);
+    let diff = mousePosition.subtract(this._initialMousePosition);
+
+    let newPosition = this._initialPosition.clone().add(diff);
+    this._knobPosition.copy(newPosition);
+    this._gradientKnobPosition.copy(newPosition)
+      .add(this._initialDistanceToGradientKnob);
+
+    this._updateStartAndEnd();
+    this._updateDOM();
+    this._ui.canvas.render();
+  }
+
+  /**
+   * Gets called when the user stops dragging the position knob
+   * @param {Event} e
+   * @private
+   */
+  _onPositionKnobUp (e) {
+    e.preventDefault();
+
+    document.removeEventListener("mousemove", this._onPositionKnobDrag);
+    document.removeEventListener("touchmove", this._onPositionKnobDrag);
+
+    document.removeEventListener("mouseup", this._onPositionKnobUp);
+    document.removeEventListener("touchend", this._onPositionKnobUp);
   }
 
   /**
@@ -130,8 +220,7 @@ class TiltShiftControls extends Control {
     e.preventDefault();
 
     this._initialMousePosition = Utils.getEventPosition(e);
-    this._initialGradientRadius = this._operation.getGradientRadius();
-    this._initialKnobPosition = this._calculateGradientKnobPosition();
+    this._initialGradientKnobPosition = this._gradientKnobPosition.clone();
 
     document.addEventListener("mousemove", this._onGradientKnobDrag);
     document.addEventListener("touchmove", this._onGradientKnobDrag);
@@ -151,20 +240,15 @@ class TiltShiftControls extends Control {
     let mousePosition = Utils.getEventPosition(e);
     let diff = mousePosition.subtract(this._initialMousePosition);
 
-    let canvasSize = this._ui.canvas.size;
-    let start = this._operation.getStart().clone().multiply(canvasSize);
-    let end = this._operation.getEnd().clone().multiply(canvasSize);
+    this._gradientKnobPosition.copy(this._initialGradientKnobPosition).add(diff);
 
-    let dist = end.clone().subtract(start);
-    let middle = start.clone().add(dist.clone().divide(2));
-
-    let newKnobPosition = this._initialKnobPosition.clone().add(diff);
-    let distFromMiddle = newKnobPosition.clone().subtract(middle);
-
-    let absoluteDist = Math.sqrt(Math.pow(distFromMiddle.x, 2) + Math.pow(distFromMiddle.y, 2));
-    let newGradientRadius = absoluteDist * 2;
+    let distance = this._gradientKnobPosition.clone().subtract(this._knobPosition);
+    let newGradientRadius = 2 * Math.sqrt(
+      Math.pow(distance.x, 2) + Math.pow(distance.y, 2)
+    );
 
     this._operation.setGradientRadius(newGradientRadius);
+    this._updateStartAndEnd();
     this._updateDOM();
     this._ui.canvas.render();
   }
@@ -185,115 +269,17 @@ class TiltShiftControls extends Control {
   }
 
   /**
-   * Gets called when the user starts dragging the knob
-   * @param {DOMElement} knob
-   * @param {Event} e
-   * @private
-   */
-  _onKnobDown (knob, e) {
-    e.preventDefault();
-
-    this._currentKnob = knob;
-    this._initialMousePosition = Utils.getEventPosition(e);
-
-    let { option } = knob.dataset;
-    let capitalized = option.charAt(0).toUpperCase() + option.slice(1);
-    this._valueBeforeDrag = this._operation[`get${capitalized}`]();
-
-    document.addEventListener("mousemove", this._onKnobDrag);
-    document.addEventListener("touchmove", this._onKnobDrag);
-
-    document.addEventListener("mouseup", this._onKnobUp);
-    document.addEventListener("touchend", this._onKnobUp);
-  }
-
-  /**
-   * Gets called while the user starts drags the knob
-   * @param {Event} e
-   * @private
-   */
-  _onKnobDrag (e) {
-    e.preventDefault();
-
-    let canvasSize = this._ui.canvas.size;
-    let mousePosition = Utils.getEventPosition(e);
-    let diff = mousePosition.subtract(this._initialMousePosition);
-
-    let { option } = this._currentKnob.dataset;
-
-    let capitalized = option.charAt(0).toUpperCase() + option.slice(1);
-
-    let newPosition = this._valueBeforeDrag.clone()
-      .multiply(canvasSize)
-      .add(diff)
-      .divide(canvasSize);
-    this._operation[`set${capitalized}`](newPosition);
-    this._updateDOM();
-    this._ui.canvas.render();
-  }
-
-  /**
-   * Gets called when the user stops dragging the knob
-   * @param {Event} e
-   * @private
-   */
-  _onKnobUp (e) {
-    e.preventDefault();
-
-    document.removeEventListener("mousemove", this._onKnobDrag);
-    document.removeEventListener("touchmove", this._onKnobDrag);
-
-    document.removeEventListener("mouseup", this._onKnobUp);
-    document.removeEventListener("touchend", this._onKnobUp);
-  }
-
-  /**
-   * Calculates the gradient knob position using the current start and end
-   * position
-   * @return {Vector2}
-   * @private
-   */
-  _calculateGradientKnobPosition () {
-    let canvasSize = this._ui.canvas.size;
-    let start = this._operation.getStart().clone().multiply(canvasSize);
-    let end = this._operation.getEnd().clone().multiply(canvasSize);
-
-    let gradientRadius = this._operation.getGradientRadius() / 2;
-    let dist = end.clone().subtract(start);
-    let middle = start.clone().add(dist.clone().divide(2));
-
-    let totalDist = Math.sqrt(Math.pow(dist.x, 2) + Math.pow(dist.y, 2));
-    let factor = dist.clone().divide(totalDist);
-
-    return middle.clone()
-      .add(-gradientRadius * factor.y, gradientRadius * factor.x);
-  }
-
-  /**
    * Updates the knob
    * @private
    */
   _updateDOM () {
-    let canvasSize = this._ui.canvas.size;
-    let start = this._operation.getStart()
-      .clone()
-      .multiply(canvasSize);
-    start.clamp(new Vector2(0, 0), canvasSize);
+    let position = this._knobPosition;
+    this._positionKnob.style.left = `${position.x}px`;
+    this._positionKnob.style.top = `${position.y}px`;
 
-    this._startKnob.style.left = `${start.x}px`;
-    this._startKnob.style.top = `${start.y}px`;
-
-    let end = this._operation.getEnd()
-      .clone()
-      .multiply(canvasSize);
-    end.clamp(new Vector2(0, 0), canvasSize);
-
-    this._endKnob.style.left = `${end.x}px`;
-    this._endKnob.style.top = `${end.y}px`;
-
-    let gradientKnobPosition = this._calculateGradientKnobPosition();
-    this._gradientKnob.style.left = `${gradientKnobPosition.x}px`;
-    this._gradientKnob.style.top = `${gradientKnobPosition.y}px`;
+    position = this._gradientKnobPosition;
+    this._gradientKnob.style.left = `${position.x}px`;
+    this._gradientKnob.style.top = `${position.y}px`;
   }
 
   /**
