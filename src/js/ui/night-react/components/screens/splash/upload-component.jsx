ebb101857ecfd2dd0ee7f2edc4626e9ea23594e6
Sascha Gehlich <sascha@gehlich.us>
2015-09-01 17:21:55 +0200

    Simple splash screen

---
+++ src/js/ui/night-react/components/screens/splash/upload-component.jsx
@@ -0,0 +1,34 @@
+/*
+ * Photo Editor SDK - photoeditorsdk.com
+ * Copyright (c) 2013-2015 9elements GmbH
+ *
+ * Released under Attribution-NonCommercial 3.0 Unported
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * For commercial use, please contact us at contact@9elements.com
+ */
+
+import { React, BEM, Classnames } from '../../../globals'
+const block = BEM.block('splashScreen')
+
+export default class UploadComponent extends React.Component {
+  render () {
+    console.log(this.context)
+    const rowElement = block.element('row')
+    const cellElement = block.element('cell')
+    const rowClassName = Classnames(
+      rowElement.str,
+      rowElement.modifier('upload').str
+    )
+    const cellClassName = Classnames(
+      cellElement.str,
+      cellElement.modifier('upload').str
+    )
+
+    return (<div className={rowClassName}>
+      <div className={cellClassName}>
+
+      </div>
+    </div>)
+  }
+}
