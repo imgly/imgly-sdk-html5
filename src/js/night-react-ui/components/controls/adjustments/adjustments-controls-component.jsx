2912266a92bdd2bcfb0e9ba625e3841b55d2a164
Sascha Gehlich <sascha@gehlich.us>
2015-09-09 12:50:02 +0200

    Created files for adjustment controls

---
+++ src/js/night-react-ui/components/controls/adjustments/adjustments-controls-component.jsx
@@ -0,0 +1,88 @@
+/** @jsx ReactBEM.createElement **/
+/*
+ * Photo Editor SDK - photoeditorsdk.com
+ * Copyright (c) 2013-2015 9elements GmbH
+ *
+ * Released under Attribution-NonCommercial 3.0 Unported
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * For commercial use, please contact us at contact@9elements.com
+ */
+
+import { ReactBEM, BaseChildComponent } from '../../../globals'
+import ScrollbarComponent from '../../scrollbar-component'
+
+import BrightnessControls from './brightness/'
+import SaturationControls from './saturation/'
+import ContrastControls from './contrast/'
+
+const ITEMS = [
+  BrightnessControls,
+  SaturationControls,
+  ContrastControls
+]
+
+export default class AdjustmentsControlsComponent extends BaseChildComponent {
+  constructor (...args) {
+    super(...args)
+
+    this._bindAll('_onBackClick')
+  }
+
+  /**
+   * Gets called when the user clicks the back button
+   * @param {Event} e
+   * @private
+   */
+  _onBackClick (e) {
+    this.props.onSwitchControls('back')
+  }
+
+  /**
+   * Gets called when the user clicks one of the three buttons
+   * @param {Object} controlsItem
+   * @param {Event} e
+   * @private
+   */
+  _onButtonClick (controlsItem, e) {
+    this.props.editor.switchToControls(controlsItem)
+  }
+
+  /**
+   * Renders this component
+   * @return {ReactBEM.Element}
+   */
+  renderWithBEM () {
+    const ui = this.context.ui
+
+    const listItems = ITEMS
+      .filter((item) => item.isSelectable(this.context.ui))
+      .map((item) => {
+        return (<li
+          bem='e:item'
+          key={item.identifier}>
+          <bem specifier='$b:controls'>
+            <div bem='$e:button m:withLabel' onClick={this._onButtonClick.bind(this, item)}>
+              <img bem='e:icon' src={ui.getHelpers().assetPath(`controls/adjustments/${item.identifier}@2x.png`, true)} />
+              <div bem='e:label'>{this._t(`controls.adjustments.${item.identifier}`)}</div>
+            </div>
+          </bem>
+        </li>)
+      })
+
+    return (<div bem='$b:controls e:table'>
+      <div bem='e:cell m:button m:withBorderRight'>
+        <div bem='$e:button' onClick={this._onBackClick}>
+          <img bem='e:icon' src={ui.getHelpers().assetPath(`controls/back@2x.png`, true)} />
+        </div>
+      </div>
+      <div bem='e:cell m:list'>
+        <ScrollbarComponent>
+          <ul bem='$e:list'>
+            {listItems}
+          </ul>
+        </ScrollbarComponent>
+      </div>
+    </div>)
+  }
+}
