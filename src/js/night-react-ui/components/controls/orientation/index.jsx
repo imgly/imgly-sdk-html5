642369706947e047be4966c538c51066a448bce8
Sascha Gehlich <sascha@gehlich.us>
2015-09-09 10:38:07 +0200

    Added orientation controls

--- src/js/night-react-ui/components/controls/orientation/index.jsx
+++ src/js/night-react-ui/components/controls/orientation/index.jsx
@@ -8,9 +8,11 @@
  * For commercial use, please contact us at contact@9elements.com
  */
 
+import OrientationControlsComponent from './orientation-controls-component'
+
 export default {
   canvasControls: null,
-  controls: null,
+  controls: OrientationControlsComponent,
   identifier: 'orientation',
   isSelectable: (ui) => {
     return ui.isOperationSelected('rotation') ||
