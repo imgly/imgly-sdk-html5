ad8ed20723b3e15fb227d11637597f44200ea3b5
Sascha Gehlich <sascha@gehlich.us>
2015-09-10 13:39:14 +0200

    Reposition / center canvas

--- src/js/night-react-ui/components/screens/editor/canvas-component.jsx
+++ src/js/night-react-ui/components/screens/editor/canvas-component.jsx
@@ -12,6 +12,14 @@
 import { React, ReactBEM, Vector2, BaseChildComponent } from '../../../globals'
 
 export default class CanvasComponent extends BaseChildComponent {
+  constructor (...args) {
+    super(...args)
+
+    this.state = {
+      canvasPosition: new Vector2()
+    }
+  }
+
   /**
    * Gets called after this component has been mounted
    */
@@ -24,6 +32,36 @@ export default class CanvasComponent extends BaseChildComponent {
     renderer.setDimensions(`${canvasDimensions.x}x${canvasDimensions.y}`)
     renderer.setCanvas(this._canvas)
     renderer.render()
+      .then(() => {
+        this._repositionCanvas()
+      })
+  }
+
+  /**
+   * Repositions the canvas
+   * @private
+   */
+  _repositionCanvas () {
+    const canvas = React.findDOMNode(this.refs.canvas)
+    const canvasCell = React.findDOMNode(this.refs.canvasCell)
+    const containerDimensions = new Vector2(canvasCell.offsetWidth, canvasCell.offsetHeight)
+    const canvasDimensions = new Vector2(canvas.width, canvas.height)
+    const canvasPosition = containerDimensions
+      .clone()
+      .divide(2)
+      .subtract(canvasDimensions.divide(2))
+    this.setState({ canvasPosition })
+  }
+
+  /**
+   * Returns the style properties for the draggable canvas area
+   * @private
+   */
+  _getDraggableStyle () {
+    return {
+      top: this.state.canvasPosition.y,
+      left: this.state.canvasPosition.x
+    }
   }
 
   /**
@@ -34,7 +72,7 @@ export default class CanvasComponent extends BaseChildComponent {
     return (
       <div bem='$b:canvas e:container e:row'>
         <div bem='e:container e:cell' ref='canvasCell'>
-          <div bem='$e:canvas m:draggable'>
+          <div bem='$e:canvas m:draggable' style={this._getDraggableStyle()}>
             <canvas ref='canvas' />
           </div>
         </div>
