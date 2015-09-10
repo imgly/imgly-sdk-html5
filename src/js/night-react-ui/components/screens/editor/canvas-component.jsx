d2875be97a0845f9b204a6b1ff6c8aebc1aec0b7
Sascha Gehlich <sascha@gehlich.us>
2015-09-10 16:26:24 +0200

    Integrated canvas dragging

--- src/js/night-react-ui/components/screens/editor/canvas-component.jsx
+++ src/js/night-react-ui/components/screens/editor/canvas-component.jsx
@@ -9,15 +9,84 @@
  * For commercial use, please contact us at contact@9elements.com
  */
 
-import { SDKUtils, React, ReactBEM, Vector2, BaseChildComponent } from '../../../globals'
+import { Utils, SDKUtils, React, ReactBEM, Vector2, BaseChildComponent } from '../../../globals'
 
 export default class CanvasComponent extends BaseChildComponent {
   constructor (...args) {
     super(...args)
 
+    this._bindAll(
+      '_onDragStart',
+      '_onDragMove',
+      '_onDragEnd'
+    )
+
     this.state = {
-      canvasPosition: new Vector2()
+      canvasPosition: new Vector2(),
+      canvasOffset: new Vector2()
     }
+  }
+
+  /**
+   * Gets called when the user starts dragging the canvas
+   * @param {React.SyntheticEvent} e
+   * @private
+   */
+  _onDragStart (e) {
+    this._dragStartPosition = Utils.getEventPosition(e.nativeEvent)
+    this._dragInitialOffset = this.state.canvasOffset.clone()
+    document.addEventListener('mousemove', this._onDragMove)
+    document.addEventListener('touchmove', this._onDragMove)
+    document.addEventListener('mouseup', this._onDragEnd)
+    document.addEventListener('touchend', this._onDragEnd)
+  }
+
+  /**
+   * Gets called while the user drags the canvas
+   * @param {DOMEvent} e
+   * @private
+   */
+  _onDragMove (e) {
+    const eventPosition = Utils.getEventPosition(e)
+    const diffFromStart = eventPosition
+      .clone()
+      .subtract(this._dragStartPosition)
+
+    const newOffset = this._dragInitialOffset
+      .clone()
+      .add(diffFromStart)
+    this._updateOffset(newOffset)
+  }
+
+  _updateOffset (offset = this.state.canvasOffset) {
+    const canvasCell = React.findDOMNode(this.refs.canvasCell)
+    const containerDimensions = new Vector2(canvasCell.offsetWidth, canvasCell.offsetHeight)
+    const canvasDimensions = new Vector2(this._canvas.width, this._canvas.height)
+    const minOffset = containerDimensions
+      .clone()
+      .subtract(canvasDimensions)
+      .divide(2)
+    const maxOffset = canvasDimensions
+      .clone()
+      .subtract(containerDimensions)
+      .divide(2)
+      .clamp(new Vector2(0, 0), null)
+
+    offset.clamp(minOffset, maxOffset)
+
+    this.setState({ canvasOffset: offset })
+  }
+
+  /**
+   * Gets called when the user stops dragging the canvas
+   * @param {DOMEvent} e
+   * @private
+   */
+  _onDragEnd (e) {
+    document.removeEventListener('mousemove', this._onDragMove)
+    document.removeEventListener('touchmove', this._onDragMove)
+    document.removeEventListener('mouseup', this._onDragEnd)
+    document.removeEventListener('touchend', this._onDragEnd)
   }
 
   /**
@@ -26,10 +95,15 @@ export default class CanvasComponent extends BaseChildComponent {
    */
   getDefaultZoom () {
     const canvasCell = React.findDOMNode(this.refs.canvasCell)
+
     const containerDimensions = new Vector2(canvasCell.offsetWidth, canvasCell.offsetHeight)
     const nativeDimensions = this.context.renderer.getNativeDimensions()
     const defaultDimensions = SDKUtils.resizeVectorToFit(nativeDimensions, containerDimensions)
-    return defaultDimensions.divide(nativeDimensions).x
+
+    // Since default and native dimensions have the same ratio, we can take either x or y here
+    return defaultDimensions
+      .divide(nativeDimensions)
+      .x
   }
 
   /**
@@ -76,6 +150,9 @@ export default class CanvasComponent extends BaseChildComponent {
   componentWillReceiveProps (nextProps) {
     if (nextProps.zoom !== this.props.zoom) {
       this._onCanvasUpdate(nextProps.zoom)
+        .then(() => {
+          this._updateOffset()
+        })
     }
   }
 
@@ -101,8 +178,8 @@ export default class CanvasComponent extends BaseChildComponent {
    */
   _getDraggableStyle () {
     return {
-      top: this.state.canvasPosition.y,
-      left: this.state.canvasPosition.x
+      top: this.state.canvasPosition.y + this.state.canvasOffset.y,
+      left: this.state.canvasPosition.x + this.state.canvasOffset.x
     }
   }
 
@@ -114,7 +191,11 @@ export default class CanvasComponent extends BaseChildComponent {
     return (
       <div bem='$b:canvas e:container e:row'>
         <div bem='e:container e:cell' ref='canvasCell'>
-          <div bem='$e:canvas m:draggable' style={this._getDraggableStyle()}>
+          <div
+            bem='$e:canvas m:draggable'
+            onTouchStart={this._onDragStart}
+            onMouseDown={this._onDragStart}
+            style={this._getDraggableStyle()}>
             <canvas ref='canvas' />
           </div>
         </div>
