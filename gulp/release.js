3c1b4dffb6358d3f49c45a2f3322fc8c7c55cbd4
Sascha Gehlich <sascha@gehlich.us>
2015-09-15 19:36:30 +0200

    `gulp build` -> `gulp release`

--- gulp/release.js
+++ gulp/release.js
@@ -2,7 +2,7 @@ var config = require('../config')
 var gulp = require('gulp')
 var gulpSequence = require('gulp-sequence')
 
-gulp.task('build', function (cb) {
+gulp.task('release', function (cb) {
   var tasks = ['clean', 'assets', 'sass', 'webpack', 'uglify:js', 'uglify:css']
 
   if (config.env === 'production') {
