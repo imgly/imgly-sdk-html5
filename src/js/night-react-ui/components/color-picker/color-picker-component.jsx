7fea8baf64417864f4012926826294c06b336022
Sascha Gehlich <sascha@gehlich.us>
2015-09-28 11:29:15 +0200

    Started styling the color picker

--- src/js/night-react-ui/components/color-picker/color-picker-component.jsx
+++ src/js/night-react-ui/components/color-picker/color-picker-component.jsx
@@ -10,17 +10,81 @@
  */
 
 import { ReactBEM, BaseChildComponent } from '../../globals'
+import ColorPickerOverlayComponent from './overlay-component'
 
 export default class ColorPickerComponent extends BaseChildComponent {
   constructor (...args) {
     super(...args)
+
+    this._bindAll('_onButtonClick')
 
     this.state = {
       overlayVisible: false
     }
   }
 
+  // -------------------------------------------------------------------------- LIFECYCLE
+
+  /**
+   * Gets called when this component has been mounted
+   */
+  componentDidMount () {
+    this._renderColor()
+  }
+
+  /**
+   * Gets called after this component has been updated
+   */
+  componentDidUpdate () {
+    this._renderColor()
+  }
+
+  // -------------------------------------------------------------------------- EVENTS
+
+  /**
+   * Gets called when the color picker button has been clicked
+   * @param  {Event} e
+   * @private
+   */
+  _onButtonClick (e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    this.setState({ overlayVisible: !this.state.overlayVisible })
+  }
+
+  // -------------------------------------------------------------------------- RENDERING
+
+  /**
+   * Renders the current color on the preview canvas
+   * @private
+   */
+  _renderColor () {
+    const canvas = this.refs.canvas.getDOMNode()
+    const context = canvas.getContext('2d')
+
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = this.props.value.toRGBA()
+    context.fillRect(0, 0, canvas.width, canvas.height)
+  }
+
+  /**
+   * Renders this component
+   * @return {ReactBEM.Element}
+   */
   renderWithBEM () {
-    return <div>ColorPicker</div>
+    let Overlay = null
+    if (this.state.overlayVisible) {
+      Overlay = (<ColorPickerOverlayComponent
+        initialValue={this.state.value} />)
+    }
+
+    return (<bem specifier='$b:controls'>
+      <div bem='$e:button m:withLabel' onClick={this._onButtonClick}>
+        <canvas bem='b:colorPicker e:preview' ref='canvas' />
+        <div bem='e:label'>Color</div>
+        {Overlay}
+      </div>
+    </bem>)
   }
 }
