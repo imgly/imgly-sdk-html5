93f0087ed04311a140c3329b64b2d2557e0c8387
Sascha Gehlich <sascha@gehlich.us>
2015-10-04 12:31:27 +0300

    Stickers are selectable from overview canvas

--- src/js/night-react-ui/components/controls/stickers/index.jsx
+++ src/js/night-react-ui/components/controls/stickers/index.jsx
@@ -15,6 +15,31 @@ export default {
   canvasControls: StickersCanvasControlsComponent,
   controls: StickersControlsComponent,
   identifier: 'stickers',
+
+  /**
+   * Checks if there is something at the given position that
+   * would cause the UI to switch to this control on click
+   * @param  {Vector2} position
+   * @param  {Object} context
+   * @return {*}
+   */
+  clickAtPosition: (position, context) => {
+    if (!context.ui.operationExists('stickers')) return false
+    const renderer = context.kit.getRenderer()
+    const operation = context.ui.getOrCreateOperation('stickers')
+    const sticker = operation.getStickerAtPosition(renderer, position)
+    if (!sticker) {
+      return false
+    } else {
+      return { selectedSticker: sticker }
+    }
+  },
+
+  /**
+   * Returns the initial state for this control
+   * @param  {Object} context
+   * @return {Object}
+   */
   getInitialSharedState: (context) => {
     const operationExistedBefore = context.ui.operationExists('stickers')
     const operation = context.ui.getOrCreateOperation('stickers')
