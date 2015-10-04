1e1e63a5725846a7904508f51d6cd8cf612d8d3d
Sascha Gehlich <sascha@gehlich.us>
2015-10-04 15:14:55 +0300

    WebGL: Use linear filtering for downscaling

--- src/js/sdk/renderers/webgl-renderer.js
+++ src/js/sdk/renderers/webgl-renderer.js
@@ -628,7 +628,7 @@ class WebGLRenderer extends BaseRenderer {
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 
     if (image) {
