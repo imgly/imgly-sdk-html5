d79f4609d5ead271f165e74a12b1307fcbfd817c
Sascha Gehlich <sascha@gehlich.us>
2015-10-20 15:44:23 +0200

    :checkered_flag: v2.0.3-8

--- src/js/imglykit.js
+++ src/js/imglykit.js
@@ -8,7 +8,7 @@
  * For commercial use, please contact us at contact@9elements.com
  */
 
-const VERSION = '2.0.3-7'
+const VERSION = '2.0.3-8'
 
 import RenderImage from './lib/render-image'
 import ImageExporter from './lib/image-exporter'
