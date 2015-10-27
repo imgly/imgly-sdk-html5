14204729c370298930fd9d332baf76f611f762ed
Sascha Gehlich <sascha@gehlich.us>
2015-10-27 11:46:51 +0100

    :bug: Remove .only from CropOperation tests

--- test/operations/crop-operation.test.js
+++ test/operations/crop-operation.test.js
@@ -27,7 +27,7 @@ beforeEach(function () {
   kit = new ImglyKit({ image: image, ui: { enabled: false } });
 });
 
-describe.only("CropOperation", function () {
+describe("CropOperation", function () {
 
   describe("#render", function () {
 
