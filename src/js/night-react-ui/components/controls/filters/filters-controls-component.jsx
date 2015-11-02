9539a73db673a1c71397dc62b4fb9bf4fe940407
Sascha Gehlich <sascha@gehlich.us>
2015-11-02 14:22:28 +0100

    :bug: Reset intensity after filter has changed

--- src/js/night-react-ui/components/controls/filters/filters-controls-component.jsx
+++ src/js/night-react-ui/components/controls/filters/filters-controls-component.jsx
@@ -71,6 +71,7 @@ export default class FiltersControlsComponent extends BaseChildComponent {
    */
   _onItemClick (filter, e) {
     this._operation.setFilter(filter)
+    this._operation.setIntensity(1)
     this._emitEvent(Constants.EVENTS.CANVAS_RENDER)
   }
 
