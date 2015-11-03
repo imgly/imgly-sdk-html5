2fda3e9899e74a5c15d62cd801f727184af3d298
Sascha Gehlich <sascha@gehlich.us>
2015-11-03 13:53:29 +0100

    :star: Remove browsersync, replace with gulp-webserver

--- gulp/serve.js
+++ gulp/serve.js
@@ -1,20 +1,13 @@
 var gulp = require('gulp')
 var config = require('../config')
-var browserSync = require('browser-sync')
+var webserver = require('gulp-webserver')
 
+console.log(config.serverPort)
 gulp.task('serve', function () {
-  return browserSync({
-    notify: false,
-    open: false,
-    server: {
-      baseDir: ['.'],
-      middleware: function (req, res, next) {
-        // res.setHeader('Content-Security-Policy', "default-src https: 'self'; connect-src https: 'self'; font-src https: 'self' data:; frame-src https: 'self'; img-src https: 'self' data:; media-src https: 'self'; object-src https: 'self'; script-src https: 'self'; style-src https: 'self' 'unsafe-inline';")
-        next()
-      }
-    },
-    ports: {
-      min: config.serverPort
-    }
-  })
+  return gulp.src('.')
+    .pipe(webserver({
+      directoryListing: true,
+      open: true,
+      port: config.serverPort
+    }))
 })
