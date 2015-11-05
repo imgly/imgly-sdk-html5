f3d631f2914ea5e3a4c4566ee996900758f5eb3b
Sascha Gehlich <sascha@gehlich.us>
2015-11-05 16:03:54 +0100

    :green_heart: Fix BrightnessOperation tests

--- test/sdk/operations/brightness-operation.test.js
+++ test/sdk/operations/brightness-operation.test.js
@@ -1,6 +1,4 @@
-/* global PhotoEditorSDK, describe, it, beforeEach */
-/*jshint -W083 */
-"use strict";
+/* global SpecHelpers, describe, it, beforeEach */
 /*
  * Copyright (c) 2013-2015 9elements GmbH
  *
@@ -10,63 +8,26 @@
  * For commercial use, please contact us at contact@9elements.com
  */
 
-var path = require("path");
-var fs = require("fs");
-var canvas = require("canvas");
-var BrightnessOperation = PhotoEditorSDK.Operations.Brightness;
-var kit, image, brightnessOperation;
+let kit
 
 beforeEach(function () {
-  image = new canvas.Image();
-  var imagePath = path.resolve(__dirname, "../assets/test.png");
-  var buffer = fs.readFileSync(imagePath);
-  image.src = buffer;
+  kit = SpecHelpers.initRenderer()
+})
 
-  kit = new PhotoEditorSDK.Renderer('canvas', { image: image, ui: { enabled: false } });
-});
+describe('BrightnessOperation', function () {
 
-describe("BrightnessOperation", function () {
+  describe('#render', function () {
 
-  describe("#render", function () {
+    it('should succeed', function () {
+      const operation = kit.createOperation('brightness')
+      kit.operationsStack.push(operation)
 
-    describe("without brightness set", function () {
+      return kit.render()
+        .should.be
+        .fulfilled
+    })
 
-      it("should fail", function (done) {
-        brightnessOperation = new BrightnessOperation(kit);
-        kit.operationsStack.push(brightnessOperation);
+  })
 
-        kit.render()
-          .then(function (result) {
-            should.not.exist(result);
-            done();
-          })
-          .catch(function (err) {
-            err.should.be.instanceOf(Error);
-            done();
-          });
-      });
+})
 
-    });
-
-    describe("with brightness set", function () {
-
-      it("should succeed", function (done) {
-        brightnessOperation = new BrightnessOperation(kit, {
-          brightness: 1.1
-        });
-        kit.operationsStack.push(brightnessOperation);
-
-        kit.render()
-          .then(function () {
-            done();
-          })
-          .catch(function (err) {
-            throw err;
-          });
-      });
-
-    });
-
-  });
-
-});
