240a257ffbede12ba964223245251bd79af689aa
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2015-11-16 21:09:57 +0100

    added normal map filter

--- src/js/sdk/operations/art/index.js
+++ src/js/sdk/operations/art/index.js
@@ -10,4 +10,4 @@
 
 exports.IdentityFilter = require('../filters/identity-filter')
 exports.OilPaintFilter = require('./oil-paint-filter')
-exports.WaterColorFilter = require('./water-color-filter')
+exports.WaterColorFilter = require('./normal-map-filter')
