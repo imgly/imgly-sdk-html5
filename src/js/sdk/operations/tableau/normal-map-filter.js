f33194591482014b5d2b9940fba715703122d077
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2015-11-24 17:53:52 +0100

    added basic test textures

--- src/js/sdk/operations/tableau/normal-map-filter.js
+++ src/js/sdk/operations/tableau/normal-map-filter.js
@@ -74,134 +74,33 @@ class NormalMapFilter extends Filter {
       var context = renderer.getContext()
       var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
       var pixels = imageData.data
+      var originalPixels = context.getImageData(0, 0, canvas.width, canvas.height).data
       var index
-      var radius = 3
-      var n = (radius + 1) * (radius + 1)
-      var k = 0
-      var l = 0
-      var i = 0
-      var j = 0
-      var original = [0, 0, 0]
       for (var y = 0; y < canvas.height; y++) {
         for (var x = 0; x < canvas.width; x++) {
-          var m0 = new Color(0.0, 0.0, 0.0, 1)
-          var m1 = new Color(0.0, 0.0, 0.0, 1)
-          var m2 = new Color(0.0, 0.0, 0.0, 1)
-          var m3 = new Color(0.0, 0.0, 0.0, 1)
-          var s0 = new Color(0.0, 0.0, 0.0, 1)
-          var s1 = new Color(0.0, 0.0, 0.0, 1)
-          var s2 = new Color(0.0, 0.0, 0.0, 1)
-          var s3 = new Color(0.0, 0.0, 0.0, 1)
-          var c = new Color(0.0, 0.0, 0.0, 1)
+          var center = new Color(0.0, 0.0, 0.0, 1)
+          var topLeft = new Color(0.0, 0.0, 0.0, 1)
+          var left = new Color(0.0, 0.0, 0.0, 1)
+          var bottomLeft = new Color(0.0, 0.0, 0.0, 1)
+          var top = new Color(0.0, 0.0, 0.0, 1)
+          var bottom = new Color(0.0, 0.0, 0.0, 1)
+          var topRight = new Color(0.0, 0.0, 0.0, 1)
+          var right = new Color(0.0, 0.0, 0.0, 1)
+          var bottomRight = new Color(0.0, 0.0, 0.0, 1)
           index = (y * canvas.width + x) * 4
-          original[0] = pixels[index]
-          original[1] = pixels[index + 1]
-          original[2] = pixels[index + 2]
-          for (j = -radius; j <= 0; ++j) {
-            for (i = -radius; i <= 0; ++i) {
-              k = x + i
-              l = y + j
-              if ((k >= 0 && k < canvas.width) && (l >= 0 && l < canvas.height)) {
-                index = (l * canvas.width + k) * 4
-                c.r = pixels[index] / 255.0
-                c.g = pixels[index + 1] / 255.0
-                c.b = pixels[index + 2] / 255.0
-                m0.add(c)
-                s0.add(c.multiply(c))
-              }
-            }
-          }
-          for (j = -radius; j <= 0; ++j) {
-            for (i = 0; i <= radius; ++i) {
-              k = x + i
-              l = y + j
-              if ((k >= 0 && k < canvas.width) && (l >= 0 && l < canvas.height)) {
-                index = (l * canvas.width + k) * 4
-                c.r = pixels[index] / 255.0
-                c.g = pixels[index + 1] / 255.0
-                c.b = pixels[index + 2] / 255.0
-                m1.add(c)
-                s1.add(c.multiply(c))
-              }
-            }
-          }
-          for (j = 0; j <= radius; ++j) {
-            for (i = 0; i <= radius; ++i) {
-              k = x + i
-              l = y + j
-              if ((k >= 0 && k < canvas.width) && (l >= 0 && l < canvas.height)) {
-                index = (l * canvas.width + k) * 4
-                c.r = pixels[index] / 255.0
-                c.g = pixels[index + 1] / 255.0
-                c.b = pixels[index + 2] / 255.0
-                m2.add(c)
-                s2.add(c.multiply(c))
-              }
-            }
-          }
-          for (j = 0; j <= radius; ++j) {
-            for (i = -radius; i <= 0; ++i) {
-              k = x + i
-              l = y + j
-              if ((k >= 0 && k < canvas.width) && (l >= 0 && l < canvas.height)) {
-                index = (l * canvas.width + k) * 4
-                c.r = pixels[index] / 255.0
-                c.g = pixels[index + 1] / 255.0
-                c.b = pixels[index + 2] / 255.0
-                m3.add(c)
-                s3.add(c.multiply(c))
-              }
-            }
-          }
-          index = (y * canvas.width + x) * 4
-          var min_sigma2 = 25500
-          m0.divide(n)
-          s0.r = Math.abs(s0.r / n - m0.r * m0.r)
-          s0.g = Math.abs(s0.g / n - m0.g * m0.g)
-          s0.b = Math.abs(s0.b / n - m0.b * m0.b)
-          var sigma2 = s0.r + s0.g + s0.b
-          if (sigma2 < min_sigma2) {
-            min_sigma2 = sigma2
-            pixels[index] = m0.r * 255.0
-            pixels[index + 1] = m0.g * 255.0
-            pixels[index + 2] = m0.b * 255.0
-          }
-          m1.divide(n)
-          s1.r = Math.abs(s1.r / n - m1.r * m1.r)
-          s1.g = Math.abs(s1.g / n - m1.g * m1.g)
-          s1.b = Math.abs(s1.b / n - m1.b * m1.b)
-          sigma2 = s1.r + s1.g + s1.b
-          if (sigma2 < min_sigma2) {
-            min_sigma2 = sigma2
-            pixels[index] = m1.r * 255.0
-            pixels[index + 1] = m1.g * 255.0
-            pixels[index + 2] = m1.b * 255.0
-          }
-          m2.divide(n)
-          s2.r = Math.abs(s2.r / n - m2.r * m2.r)
-          s2.g = Math.abs(s2.g / n - m2.g * m2.g)
-          s2.b = Math.abs(s2.b / n - m2.b * m2.b)
-          sigma2 = s2.r + s2.g + s2.b
-          if (sigma2 < min_sigma2) {
-            min_sigma2 = sigma2
-            pixels[index] = m2.r * 255.0
-            pixels[index + 1] = m2.g * 255.0
-            pixels[index + 2] = m2.b * 255.0
-          }
-          m3.divide(n)
-          s3.r = Math.abs(s3.r / n - m3.r * m3.r)
-          s3.g = Math.abs(s3.g / n - m3.g * m3.g)
-          s3.b = Math.abs(s3.b / n - m3.b * m3.b)
-          sigma2 = s3.r + s3.g + s2.b
-          if (sigma2 < min_sigma2) {
-            min_sigma2 = sigma2
-            pixels[index] = m3.r * 255.0
-            pixels[index + 1] = m3.g * 255.0
-            pixels[index + 2] = m3.b * 255.0
-          }
-          pixels[index] = (1 - this._intensity) * original[0] + this._intensity * pixels[index]
-          pixels[index + 1] = (1 - this._intensity) * original[1] + this._intensity * pixels[index + 1]
-          pixels[index + 2] = (1 - this._intensity) * original[2] + this._intensity * pixels[index + 2]
+          center[0] = originalPixels[index]
+          center[1] = originalPixels[index + 1]
+          center[2] = originalPixels[index + 2]
+
+          index = ((y - 1) * canvas.width + (x - 1)) * 4
+          topLeft[0] = originalPixels[index]
+          topLeft[1] = originalPixels[index + 1]
+          topLeft[2] = originalPixels[index + 2]
+
+          index = ((y - 1) * canvas.width + (x - 1)) * 4
+          topLeft[0] = originalPixels[index]
+          topLeft[1] = originalPixels[index + 1]
+          topLeft[2] = originalPixels[index + 2]
         }
       }
       context.putImageData(imageData, 0, 0)
