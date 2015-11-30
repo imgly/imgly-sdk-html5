4f1ec680d369cf0fdefe47afc529b0a4612e6d79
Sascha Gehlich <sascha@gehlich.us>
2015-11-30 06:47:10 +0100

    Don't open browser when server starts

--- gulp/serve.js
+++ gulp/serve.js
@@ -6,7 +6,7 @@ gulp.task('serve', function () {
   return gulp.src('.')
     .pipe(webserver({
       directoryListing: true,
-      open: true,
+      open: false,
       host: '0.0.0.0',
       port: config.serverPort
     }))
